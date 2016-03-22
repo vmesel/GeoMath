3bfd61a9cf78a622d6b63ca19cf10225360d67b0
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2016-03-22 00:06:49 -0300

    Update geocalc.py
    Cara, como você me pediu uma ajuda e esse foi o único código que eu achei por aqui, tentei criar uma classe de pontos, sua representação e a distancia. Pode ir me mandando mais e-mails e a gente vai implementando mais coisa.

--- geomath/geocalc.py
+++ geomath/geocalc.py
@@ -1,28 +1,37 @@
 """
 GeoMath Calculations Module
 Originally written by: Vinicius Mesel
-Last Modification: 03/21/2016
+Fist Modification: 03/21/2016
+Last Modification: 22/02/2016 #Z4r4tu5tr4
 """
-import math
 
-# Distance of Two Points A(Xa, Ya) and B(Xb, Yb)
-def pointsdistance(A, B, round):
-    x = A[0]
-    y = A[1]
-    z = B[0]
-    w = B[1]
-    Rounding = round
-    # X = X0 and Z = X1
-    def xDistanceCalc(x,z): return (z - x)**2
-    # Y = Y0 and W = Y1
-    def yDistanceCalc (y,w): return (w - y)**2
-    #def DistanceCalc(xResult, yResult): return()
-    RealDistance = math.sqrt(yDistanceCalc (y,w) + xDistanceCalc(x,z))
-    if Rounding == True:
-        RealDistance = RealDistance*100
-        return(math.ceil(RealDistance)/100)
+from math import sqrt
 
-    return RealDistance
+"""
+ex distance:
+	>>> point = Point(1,2)
+	>>> point2 = Point(2,1)
+	>>> point.distance(point2)
+			#out: 1.4142135623730951
+ex representation:
+	>>> point = Point(1,2)
+	>>> print(point)
+		#out Point(1,2)
 
-def barycenter(PointsList):
-    return(PointsList)
+ex introspection:
+	>>> type(point)
+		#out Point
+"""
+
+class Point:
+	"Object type cartesian Point"
+	def __init__(self, x,y):
+		self.x = x
+		self.y = y
+
+	def __repr__(self):
+		return (("Point(%s, %s)") % ( self.x, self.y))
+	
+	def distance(self, point_two):
+		return sqrt(
+				pow(point_two.x - self.x, 2) + pow(point_two.y - self.y, 2) )
