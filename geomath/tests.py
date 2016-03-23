6fbabff1fac882d4a419433565cf19cbea4b0888
Vinicius Mesel (@vmesel) <vinicius@bigsp.com.br>
2016-03-23 15:07:08 -0300

    Removed the tests dependency

--- geomath/tests.py
+++
@@ -1,8 +0,0 @@
-from geocalc import Point, Area
-from nose import with_setup # optional
-
-def testing():
-    assert Point(1,2) == "Point(1,2)"
-    assert Point(9,8).distance(Point(1,2)) == 10.0
-    assert Point(9,8).midpoint(Point(1,2)) == "(5.0, 5.0)"
-    assert Area.squarearea(4) == 16
