bdb806529211706e5547c73037a2a30defa8ff5f
Wellington <wellington30071997@gmail.com>
2016-05-01 00:16:41 -0300

    Refactoring and implementation tests

--- geomath/tests.py
+++ geomath/tests.py
@@ -1,35 +1,74 @@
 # Defining tests for the TravisCI and Build Checking
-from nose import with_setup  # optional
 import geocalc as gc
 import unittest
 
 
 class PointTest(unittest.TestCase):
 
-    def test_two(self):
+    def test_distance(self):
         assert(gc.Point(9, 8).distance(gc.Point(1, 2)) == 10)
 
-    def test_three(self):
+    def test_midpoint(self):
         assert(str(gc.Point(9, 8).midpoint(gc.Point(1, 2)) == "(5.0, 5.0)"))
 
-    def test_four(self):
-        assert(gc.Figure.squarearea(4) == 16)
+    def test_quadrant(self):
+        assert(gc.Point(2, -8).quadrant() == 4)
 
-    def test_five(self):
-        assert(gc.Figure.barycenter([gc.Point(1, 2), gc.Point(2, 3), gc.Point(
-            4, 5)]) == "Point(2.3333333333333335, 2.3333333333333335)")
+    def test_line_equation(self):
+        p1 = gc.Point(2, 2)
+        p2 = gc.Point(4, 4)
+        line = gc.Line(p1, p2)
 
-    def test_coef_angular1(self):
-        assert(Line.coef_angular(0, 1, 0, 1) == 1)
+        assert(type(line.lineEquation()) == dict)
 
-    def test_coef_angular2(self):
-        assert(Line.coef_angular(0, -1, 0, 1) == -1)
+    def test_x_equation(self):
+        p1 = gc.Point(2, 2.5)
+        p2 = gc.Point(4, 4)
+        line = gc.Line(p1, p2)
 
-    def test_coef_linear1(self):
-        assert(Line.coef_linear(1, 0, 0) == 0)
+        assert(line.equationX == (-1.5))
 
-    def test_coef_linear2(self):
-        assert(Line.coef_linear(-1, 0, 0) == 0)
+    def test_y_equation(self):
+        p1 = gc.Point(2, 2.5)
+        p2 = gc.Point(4, 4)
+        line = gc.Line(p1, p2)
+
+        assert(line.equationY == 2.0)
+
+    def test_b_equation(self):
+        p1 = gc.Point(2, 2.5)
+        p2 = gc.Point(4, 4)
+        line = gc.Line(p1, p2)
+
+        assert(line.equationB == (-2.0))
+
+    def test_is_vertical(self):
+        p1 = gc.Point(2, 2)
+        p2 = gc.Point(2, 4)
+        line = gc.Line(p1, p2)
+
+        assert(line.isVertical() == True)
+
+    def test_is_horizontal(self):
+        p1 = gc.Point(2, 2)
+        p2 = gc.Point(4, 2)
+        line = gc.Line(p1, p2)
+
+        assert(line.isHorizontal() == True)
+
+    def test_coef_angular(self):
+        p1 = gc.Point(2, 2)
+        p2 = gc.Point(4, 4)
+        line = gc.Line(p1, p2)
+
+        assert(line.angularCoefficient == 1)
+
+    def test_coef_linear(self):
+        p1 = gc.Point(2, 2)
+        p2 = gc.Point(4, 4.5)
+        line = gc.Line(p1, p2)
+
+        assert(line.linearCoefficient == (-0.5))
 
 if __name__ == '__main__':
     unittest.main()
