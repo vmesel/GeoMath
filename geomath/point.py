aad519ffd24113575aac355de78b18a7edc423ad
rg3915 <rg3915@yahoo.com.br>
2016-05-11 17:31:25 -0300

    Distance between two points with mathematical notation

--- geomath/point.py
+++ geomath/point.py
@@ -1,4 +1,6 @@
-from math import sqrt
+from math import sqrt, hypot
+
+
 class Point:
     "Point Object in GeoMath library"
 
@@ -13,10 +15,10 @@ class Point:
         :param: PointTwo - Point subclass
         :return: Point subclass
         '''
-        Xm = ((self.x + PointTwo.x)/2)
-        Ym = ((self.y + PointTwo.y)/2)
+        Xm = ((self.x + PointTwo.x) / 2)
+        Ym = ((self.y + PointTwo.y) / 2)
 
-        return(Point(Xm, Ym))
+        return Point(Xm, Ym)
 
     # Euclidean distance between "self" and "PointTwo".
     def distance(self, PointTwo=None):
@@ -32,20 +34,31 @@ class Point:
         Xd = pow(PointTwo.x - self.x, 2)
         Yd = pow(PointTwo.y - self.y, 2)
 
-        return(sqrt(Xd + Yd))
+        return sqrt(Xd + Yd)
 
-    # The quadrant of point  "self".
+    def distance2(self, other):
+        '''
+        Usage:
+        >>> p1=Point(0,3)
+        >>> p2=Point(0,-3)
+        >>> Point.distance2(p1,p2)
+        '''
+        x = other.x - self.x
+        y = other.y - self.y
+        return hypot(x, y)
+
+        # The quadrant of point  "self".
     def quadrant(self):
         if self.x > 0 and self.y > 0:
-            return(1)
+            return 1
         elif self.x < 0 and self.y > 0:
-            return(2)
+            return 2
         elif self.x < 0 and self.y < 0:
-            return(3)
+            return 3
         elif self.x > 0 and self.y < 0:
-            return(4)
+            return 4
         else:
-            return(0)
+            return 0
 
     # Defines the output of the point
     def __repr__(self):
