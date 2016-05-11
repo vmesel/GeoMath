d4c36b3fe274432348ba91bcfb9de10a2030ea4f
rg3915 <rg3915@yahoo.com.br>
2016-05-11 17:37:21 -0300

    Fix typo

--- geomath/point.py
+++ geomath/point.py
@@ -18,7 +18,7 @@ class Point:
         Xm = ((self.x + PointTwo.x) / 2)
         Ym = ((self.y + PointTwo.y) / 2)
 
-        return Point(Xm, Ym)
+        return(Point(Xm, Ym))
 
     # Euclidean distance between "self" and "PointTwo".
     def distance(self, PointTwo=None):
@@ -34,7 +34,7 @@ class Point:
         Xd = pow(PointTwo.x - self.x, 2)
         Yd = pow(PointTwo.y - self.y, 2)
 
-        return sqrt(Xd + Yd)
+        return(sqrt(Xd + Yd))
 
     def distance2(self, other):
         '''
@@ -45,20 +45,20 @@ class Point:
         '''
         x = other.x - self.x
         y = other.y - self.y
-        return hypot(x, y)
+        return(hypot(x, y))
 
         # The quadrant of point  "self".
     def quadrant(self):
         if self.x > 0 and self.y > 0:
-            return 1
+            return(1)
         elif self.x < 0 and self.y > 0:
-            return 2
+            return(2)
         elif self.x < 0 and self.y < 0:
-            return 3
+            return(3)
         elif self.x > 0 and self.y < 0:
-            return 4
+            return(4)
         else:
-            return 0
+            return(0)
 
     # Defines the output of the point
     def __repr__(self):
