341a45dfb5df3fb06504cf851afe6c9f8e33fbbb
Vinicius Mesel (@vmesel) <vinicius@bigsp.com.br>
2016-05-12 19:23:08 -0300

    Line Optimized

--- geomath/line.py
+++ geomath/line.py
@@ -4,99 +4,42 @@ from math import sqrt
 import re
 
 class Line:
-    def __init__(self, PointOne=None, PointTwo=None):
-        '''
-        :param: PointOne - First point attached to the line
-        :param: PointTwo - Second point attached to the line
-        '''
+    def __init__(self):
+        self.PointOne = 0
+        self.PointTwo = 0
+        self.Angular = 0
+        self.A = 0
+        self.B = 0
+        self.C = 0
+        self.createdby = None
+
+    def Create(self,PointOne, PointTwo):
         self.PointOne = PointOne
         self.PointTwo = PointTwo
+        self.createdby = "points"
 
-    def EquationInput(self, equation):
-        try:
-            regx = re.compile(r'(\W?\d+)\S(\W\d+)\S(\W\d+)\=(\W?\d+)')
-            values = regx.findall(equation)
-            values = values[0]
+    def CreateViaEquation(self, equation):
+        self.createdby = "equation"
+        regx = re.compile(r'(\W?\d+)\S(\W\d+)\S(\W\d+)\=(\W?\d+)')
+        values = regx.findall(equation)
+        values = values[0]
 
-            print(values)
-            self.ACoefficient = float(values[0])
-            self.BCoefficient = float(values[1])
-            self.CCoefficient = float(values[2])
-            
-            return True
-        except ValueError:
-            return False
-
-    def Equation(self):
-        if self.CCoefficient == 0:
-            respGeneral = str(self.ACoefficient)+'x + '+str(self.BCoefficient)+'y = 0'
-        else:
-            respGeneral = str(self.ACoefficient)+'x + '+str(self.BCoefficient)+'y + '+str(self.CCoefficient)+' = 0'
-
-        return(respGeneral)
-
-    @property
-    def ACoefficient(self):
-        '''
-        Return the A coefficient(the one in the X right side) in the general equation of the line
-        '''
-        return(self.PointOne.y - self.PointTwo.y)
-
-    @ACoefficient.setter
-    def ACoefficient(self, newValue):
-        self.ACoefficient = newValue
-
-    @property
-    def BCoefficient(self):
-        '''
-        Return the B coefficient(the one who is aligned with Y) in the general equation of the line
-        '''
-        return(self.PointTwo.x - self.PointOne.x)
-
-    @BCoefficient.setter
-    def BCoefficient(self, newValue):
-        self.BCoefficient = newValue
-
-    @property
-    def CCoefficient(self):
-        '''
-        :return: Return the coefficient of C in the general equation of the line. Example( x + y + c = 0)
-        '''
-        return((self.PointOne.x * self.PointTwo.y) - (self.PointOne.y * self.PointTwo.x))
-
-    @CCoefficient.setter
-    def CCoefficient(self, newValue):
-        self.CCoefficient = newValue
-
-    @property
-    def AngularCoefficient(self):
-        try:
-            deltaY = (self.PointTwo.y - self.PointOne.y)
-            deltaX = (self.PointTwo.x - self.PointOne.x)
-
-            return(deltaY/deltaX)
-        except ZeroDivisionError:
-            raise ZeroDivisionError('[%s error] Slope does not exist.' % (self.__class__.__name__))
-
-    @AngularCoefficient.setter
-    def AngularCoefficient(self, newValue):
-        self.AngularCoefficient = newValue
-
-    @property
-    def LinearCoefficient(self):
-        return(self.PointOne.y - (self.AngularCoefficient * self.PointOne.x))
-
-    @LinearCoefficient.setter
-    def LinearCoefficient(self, newValue):
-        self.LinearCoefficient = newValue
+        print(values)
+        self.A = float(values[0])
+        self.B = float(values[1])
+        self.C = float(values[2])
 
     def __repr__(self):
-        return(('%s(%s, %s)') % (self.__class__.__name__, self.PointOne, self.PointTwo))
+        if self.createdby == "equation":
+            return("A: {} B: {} C: {}".format(self.A, self.B, self.C))
+        elif self.createdby == "points":
+            return("Line({},{})".format(self.PointOne, self.PointTwo))
 
     # HERE THE LINE ONLY ATTRIBUTES END
 
-    def PointDistance(self, PointOne):
-        A = ((self.ACoefficient * PointOne.x) + (self.BCoefficient * PointOne.y) + self.CCoefficient)
-        B = sqrt((self.ACoefficient * self.ACoefficient) + (self.BCoefficient * self.BCoefficient))
+    def PointDistance(self, PointTwo):
+        A = ((self.A * PointTwo.x) + (self.B * PointTwo.y) + self.C)
+        #return(A)
+        B = sqrt((self.A * self.A) + (self.B * self.B))
         solved = A / B
         return(solved)
