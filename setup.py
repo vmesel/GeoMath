02b2e82d651222eec8e883000cee6bfd2b294439
Vinicius Mesel <vinicius@bigsp.com.br>
2016-05-16 22:49:42 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,7 @@ from distutils.core import setup
 setup(
   name = 'geomath',
   packages = ['geomath'],
-<<<<<<< HEAD
   version = '0.1.2.3',
-=======
-  version = '0.1.2.2',
->>>>>>> 535494f4d0850f230205b6d0cdb48b385dc9927a
   description = 'A Mathematic Library to Handle Eucliedan Geometry',
   author = 'Vinicius Mesel',
   author_email = 'me@vmesel.com',
