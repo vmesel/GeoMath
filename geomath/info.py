8e896470171b18ff69ccdfc4c182b277c67c0d6b
Vinicius Mesel <vinicius@bigsp.com.br>
2016-05-16 22:45:36 -0300

    Info and new Setup available

---
+++ geomath/info.py
@@ -0,0 +1,9 @@
+# This has nothing to do with the library, these are just
+# Some informations you need to know
+
+def about():
+    return("GEOMATH is a Library developed for handling\
+    Analytical Geometry on Python 3.X")
+
+def version():
+    return("Version: 0.1.2.3")
