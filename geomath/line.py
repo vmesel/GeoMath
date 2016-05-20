1a7b3f1a5da39c5013b7844bf4ca06dcde00b2ff
Wellington <wellington30071997@gmail.com>
2016-05-20 11:01:51 -0300

    Implementation of the method create via_slope

--- geomath/line.py
+++ geomath/line.py
@@ -13,6 +13,9 @@ class Line:
 
             >>> line = l.Line("-8x+6y-8=0")
             A: -8 B: +6 C: -8
+
+            >>> line = l.Line(p.Point(4, 4.5), m=1)
+            Angular:1.0 Linear:0.5
         """
         #These are the initial line parameters
         self.PointOne = 0
@@ -31,9 +34,7 @@ class Line:
 
         if len(args)==1:
             if isinstance(args[0], p.Point) and "m" in kwargs:
-                # Calculate line from the slope
-                # Example using: line = l.Line(p.Point(2,2), m=4)
-                pass
+                self.create_via_slope(args[0], kwargs['m'])
             
             elif type(args[0]) is str:
                 self.create_via_equation(args[0])
@@ -42,13 +43,19 @@ class Line:
         """
         This function is for getting a line created by two points
         """
+        self.createdby = "points"
         self.PointOne = PointOne
         self.PointTwo = PointTwo
-        self.createdby = "points"
         self.A = float(self.PointOne.y - self.PointTwo.y)
         self.B = float(self.PointTwo.x - self.PointOne.x)
+        self.Angular = float(((self.PointTwo.y - self.PointOne.y) / (self.PointTwo.x - self.PointOne.x)))
+        self.Angulation = degrees(atan(float(self.Angular)))
+        
+        self.Linear = float(self.PointOne.y - (self.Angular * self.PointOne.x))
+
         if self.B > 0 or self.B == 0:
             self.B = "+" + str(self.B)
+        
         self.C = ((self.PointOne.x * self.PointTwo.y) - (self.PointOne.y * self.PointTwo.x))
         if self.C > 0 or self.C == 0:
             self.C = "+" + str(self.C)
@@ -58,8 +65,8 @@ class Line:
         This function is for getting a line created by a line equation
         """
         self.createdby = "equation"
-        regx = re.compile(r'(\W?\d+)\S(\W\d+)\S(\W\d+)\=(\W?\d+)')
-        values = regx.findall(equation)
+        regex = re.compile(r'(\W?\d+)\S(\W\d+)\S(\W\d+)\=(\W?\d+)')
+        values = regex.findall(equation)
         values = values[0]
         self.A = values[0]
         self.B = values[1]
@@ -69,9 +76,25 @@ class Line:
         self.PointOne = p.Point(float(self.C) / float(self.A),0)
         self.PointTwo = p.Point(0, float(self.C) / float(self.B))
 
+    def create_via_slope(self, PointOne, slope):
+        """
+        This function is to get a line created by a point and a slope
+        """
+        self.createdby = "slope"
+        self.PointOne = PointOne
+        self.Angular = slope
+        self.Angulation = degrees(atan(float(self.Angular)))
+
+        self.Linear = self.PointOne.y-(self.Angular*self.PointOne.x)
+
+        if self.Linear > 0 or self.Linear == 0:
+            self.Linear = "+" + str(self.Linear)
 
     def equation(self):
-        return("{}x{}y{}=0".format(self.A, self.B, self.C))
+        if self.createdby == "equation" or self.createdby == "points":
+            return("{}x{}y{}=0".format(self.A, self.B, self.C))
+        elif self.createdby == "slope":
+            return("y={}x{}".format(self.Angular, self.Linear))
 
     def get_angle(self):
         return(self.Angulation)
@@ -81,7 +104,8 @@ class Line:
             return("A: {} B: {} C: {}".format(self.A, self.B, self.C))
         elif self.createdby == "points":
             return("Line({}, {})".format(self.PointOne, self.PointTwo))
-
+        elif self.createdby == "slope":
+            return("Angular:{} Linear:{}".format(float(self.Angular), float(self.Linear)))
     # HERE THE LINE ONLY ATTRIBUTES END
 
     def point_distance(self, PointTwo):
