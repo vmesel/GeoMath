62b2d6562222f6f5cd57b2357171dc789b90e944
Vinicius Mesel <vinicius@bigsp.com.br>
2016-05-26 11:25:19 -0300

    Update Install-Geomath.sh

--- Install-Geomath.sh
+++ Install-Geomath.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 
-echo "DIGITE 1 PARA INSTALAR PELO GITHUB, DIGITE 2 PARA INSTAALAR PELO PIP!"
+echo "TYPE 1 TO INSTALL VIA GITHUB CLONING, TYPE 2 TO INSTALL BY PIP!"
 read inputUser
 if [ $inputUser = "1" ]; then
+	echo "Cloning the repository locally, so you can start developing"
 	git clone https://github.com/vmesel/GeoMath.git
 else
+	echo "Installing geomath library, it will be available via PyPi"
 	pip install geomath
 fi
 
