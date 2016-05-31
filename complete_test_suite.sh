e03657fcd197b0cb8c011a6d3aa5faf4e0650c10
Vinicius Mesel <vinicius@bigsp.com.br>
2016-05-31 18:30:01 -0300

    Update complete_test_suite.sh

--- complete_test_suite.sh
+++ complete_test_suite.sh
@@ -8,7 +8,7 @@ coverage html -d coverage
 
 rm -f pep8.log pyflakes.log
 
-./test.py
+nosetests --with-xcoverage --with-xunit
 
 pep8 --max-line-length=120 geomath > pep8.log || true
 pyflakes geomath > pyflakes.log || true
