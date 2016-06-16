02bea504c473f63737f329f8be689e6817b253ce
Gabriel Almeida de Souza <gfth4x07@hotmail.com>
2016-06-16 13:34:51 -0300

    More functions

--- geomath/vector.py
+++ geomath/vector.py
@@ -1,14 +1,16 @@
-#Vector file for the GeoMath library
+# Vector file for the GeoMath library
+from math import sqrt, acos
 from geomath import general as g
 from geomath import point as p
+# TODO: implement the ZeroVector
+
+
 class Vector:
-    """Vector Object in GeoMath library"""
-    
-    def __init__(self, a, b = None):
+    def __init__(self, a, b=None):
         # i and j by caninical base
         self.i = 0
         self.j = 0
-        if type(a) is p.Point and b == None:
+        if type(a) is p.Point and b is None:
             self.i = a.x
             self.j = a.y
         elif type(a) in g.numbers and type(b) in g.numbers:
@@ -17,22 +19,53 @@ class Vector:
         elif type(a) is p.Point and type(b) is p.Point:
             self.i = b.x - a.x
             self.j = b.y - a.y
+        self.components = (self.i, self.j)
+        self.length = sqrt(self.i**2 + self.j**2)
 
     def sum(self, v):
         """
-        This function returns sum of a giving vector
+        This function returns sum of a giving vector.
         :param: v - Vector subclass
         :return: Vector subclass
-        '''
         """
-        return Vector(self.i+v.i, self.j+v.j)  # This work with points 
-        
+        return Vector(self.i+v.i, self.j+v.j)
+
     def multiplication(self, n):
         """
         This function returns multiplication of vector by number
         :param: n - number
         :return: Vector subclass
-        '''
         """
         return Vector(n*self.i, n*self.j)
-        
+
+    def equal(self, v):
+        """
+        This function returns if this vector is equal of a giving vector.
+        :param: v - Vector subclass
+        :return: Boolean subclass
+        """
+        return self.i == v.i and self.j == v.j
+
+    def parallel(self, v):
+        """
+        This function returns if a vector is parallel of a giving vector.
+        :param: v - Vector subclass
+        :return: Boolean subclass
+        """
+        return self.i/v.i == self.j/v.j
+
+    def scalar_product(self, v):
+        """
+        :param: v - Vector subclass
+        :return: number
+        """
+        return (self.i*v.i + self.j*v.j)
+
+    def angle(self, v):
+        """
+        This function returns the angle between this vector
+         and a giving vector.
+        :param: v - Vector subclass
+        :return: Boolean subclass
+        """
+        return acos(scalar_product(self, v)/(self.length*v.length))
